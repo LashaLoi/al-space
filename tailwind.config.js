a0360db2e861ba8e017f99a3d8104261f00cd619
Alex <lashaloi1409@gmail.com>
2022-02-22 01:33:46 +0300

    fix vercel deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,38 +5,7 @@ module.exports = {
     './modules/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      typography: (theme) => ({
-        default: {
-          css: {
-            pre: {
-              color: theme('colors.grey.1000'),
-              backgroundColor: theme('colors.grey.100'),
-            },
-            'pre code::before': {
-              'padding-left': 'unset',
-            },
-            'pre code::after': {
-              'padding-right': 'unset',
-            },
-            code: {
-              backgroundColor: theme('colors.grey.100'),
-              color: '#DD1144',
-              fontWeight: '400',
-              'border-radius': '0.25rem',
-            },
-            'code::before': {
-              content: '""',
-              'padding-left': '0.25rem',
-            },
-            'code::after': {
-              content: '""',
-              'padding-right': '0.25rem',
-            },
-          },
-        },
-      }),
-    },
+    extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
   darkMode: 'class',

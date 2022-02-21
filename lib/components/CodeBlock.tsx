52ddb37487f84b3bacec4965d3e7a2d01a0b05ca
Alex <lashaloi1409@gmail.com>
2022-02-22 01:35:33 +0300

    remove extra components

--- lib/components/CodeBlock.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-
-export function CodeBlock({ ...props }) {
-  return <SyntaxHighlighter language="ts" {...props} />
-}
